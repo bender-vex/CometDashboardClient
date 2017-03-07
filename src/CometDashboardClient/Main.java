eb799f6da2954bcc4ec7ef7ed39ebaea8061adb1
JosephFoster118 <Joseph.Foster118@gmail.com>
2017-03-06 22:47:23 -0600

    Started this poops

---
+++ src/CometDashboardClient/Main.java
@@ -0,0 +1,15 @@
+package CometDashboardClient;
+
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Comet Dashboard Client");
+		
+	}
+
+}
---
+++ src/CometDashboardClient/Module.java
@@ -0,0 +1,14 @@
+package CometDashboardClient;
+
+import javax.swing.JPanel;
+
+public class Module extends JPanel
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String type;
+
+}
